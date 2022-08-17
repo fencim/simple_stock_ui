c963b4a107ceafd99ee7ca5b446b124933947a79
fencim <fencim@gmail.com>
2022-08-17 19:43:37 +0800

    apply lessons on day 3

---
+++ lib/home_menu.dart
@@ -0,0 +1,8 @@
+class HomeMenu {
+  final String name;
+  final String icon;
+  HomeMenu({
+    required this.name,
+    required this.icon,
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,28 +32,28 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   //preloading icons
-  Map<String, Icon> iconMap = {
-    'person': const Icon(Icons.person),
-    'settings': const Icon(Icons.settings),
-    'notes_sharp': const Icon(Icons.receipt_long_sharp),
-    'favorite': const Icon(Icons.favorite),
-    'pending': const Icon(Icons.pending),
-    'drive_file_rename_outline_rounded':
-        const Icon(Icons.drive_file_rename_outline_rounded),
-    'groups': const Icon(Icons.groups)
-  };
-  List<HomeMenu> profileMenu = [
-    HomeMenu(name: 'Personal Data', icon: 'person'),
-    HomeMenu(name: 'Settings', icon: 'settings'),
-    HomeMenu(name: 'E-Statement', icon: 'notes_sharp'),
-    HomeMenu(name: 'Referal Code', icon: 'favorite')
-  ];
+  // Map<String, IconData> iconMap = {
+  //   'person': const (Icons.person),
+  //   'settings': const (Icons.settings),
+  //   'notes_sharp': const (Icons.receipt_long_sharp),
+  //   'favorite': const (Icons.favorite),
+  //   'pending': const (Icons.pending),
+  //   'drive_file_rename_outline_rounded':
+  //       const (Icons.drive_file_rename_outline_rounded),
+  //   'groups': const (Icons.groups)
+  // };
+  // List<HomeMenu> profileMenu = [
+  //   HomeMenu(name: 'Personal Data', icon: 'person'),
+  //   HomeMenu(name: 'Settings', icon: 'settings'),
+  //   HomeMenu(name: 'E-Statement', icon: 'notes_sharp'),
+  //   HomeMenu(name: 'Referal Code', icon: 'favorite')
+  // ];
 
-  List<HomeMenu> appMenu = [
-    HomeMenu(name: 'FAQs', icon: 'pending'),
-    HomeMenu(name: 'Our Handbook', icon: 'drive_file_rename_outline_rounded'),
-    HomeMenu(name: 'Community', icon: 'groups')
-  ];
+  // List<HomeMenu> appMenu = [
+  //   HomeMenu(name: 'FAQs', icon: 'pending'),
+  //   HomeMenu(name: 'Our Handbook', icon: 'drive_file_rename_outline_rounded'),
+  //   HomeMenu(name: 'Community', icon: 'groups')
+  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -63,174 +64,178 @@ class _HomePageState extends State<HomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        body: Column(
-      children: [
-        Row(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(
-                left: 16,
-                top: 16,
-              ),
-              height: 64,
-              width: 64,
-              child: const CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://media-exp1.licdn.com/dms/image/C5603AQH0N150mG0GIg/profile-displayphoto-shrink_200_200/0/1617267809315?e=2147483647&v=beta&t=u-1yUx5xYTkSEisAt7XT1LuKwzJ0F4iNZEtT3fZaQos'),
-              ),
-            ),
-            Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                    left: 16,
-                    top: 16,
-                  ),
-                  height: 16,
-                  width: 200,
-                  child: const Text(
-                    'Lucman M. Abdulrachman',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(
-                    left: 16,
-                    top: 8,
-                  ),
-                  height: 16,
-                  width: 170,
-                  child: const Text('University Instructor I'),
-                ),
-              ],
-            )
-          ],
-        ),
-        const SizedBox(height: 16),
-        const Divider(height: 1.15),
-        for (HomeMenu item in profileMenu) ...[
-          const SizedBox(height: 16),
-          GestureDetector(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    const SizedBox(
-                      width: 16,
-                    ),
-                    Container(
-                      height: 48,
-                      width: 48,
-                      color: Colors.transparent,
-                      child: iconMap[item.icon],
-                    ),
-                    const SizedBox(
-                      width: 16,
-                    ),
-                    Container(
-                      height: 14,
-                      width: 120,
-                      color: Colors.transparent,
-                      child: Text(item.name,
-                          style: const TextStyle(
-                              fontSize: 12, fontWeight: FontWeight.bold)),
-                    ),
-                  ],
-                ),
-                Container(
-                  margin: const EdgeInsets.only(right: 16),
-                  height: 12,
-                  width: 12,
-                  color: Colors.transparent,
-                  child: const Icon(Icons.arrow_right),
-                ),
-              ],
-            ),
-          )
-        ],
-        const SizedBox(height: 16),
-        const Divider(height: 1.15),
-        for (HomeMenu item in appMenu) ...[
-          const SizedBox(height: 16),
-          GestureDetector(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    const SizedBox(
-                      width: 16,
-                    ),
-                    Container(
-                      height: 48,
-                      width: 48,
-                      color: Colors.transparent,
-                      child: iconMap[item.icon],
-                    ),
-                    const SizedBox(
-                      width: 16,
-                    ),
-                    Container(
-                      height: 14,
-                      width: 120,
-                      color: Colors.transparent,
-                      child: Text(item.name,
-                          style: const TextStyle(
-                              fontSize: 12, fontWeight: FontWeight.bold)),
-                    ),
-                  ],
-                ),
-                Container(
-                  margin: const EdgeInsets.only(right: 16),
-                  height: 12,
-                  width: 12,
-                  color: Colors.transparent,
-                  child: const Icon(Icons.arrow_right),
-                ),
-              ],
-            ),
-          )
-        ],
-        Card(
-          margin: const EdgeInsets.only(right: 16, left: 16),
-          color: Colors.lightBlue,
-          child: Row(
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          Row(
             children: [
               Container(
-                  margin: const EdgeInsets.only(left: 25, bottom: 16, top: 16),
-                  height: 12,
-                  width: 12,
-                  color: Colors.transparent,
-                  child: const Icon(Icons.support_agent_outlined)),
-              Container(
-                margin: const EdgeInsets.only(left: 16),
-                height: 16,
-                width: 120,
-                color: Colors.transparent,
-                child: const Text('Feel free to ask. We are ready to help.'),
+                margin: const EdgeInsets.only(
+                  left: 16,
+                  top: 16,
+                ),
+                height: 64,
+                width: 64,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                      'https://media-exp1.licdn.com/dms/image/C5603AQH0N150mG0GIg/profile-displayphoto-shrink_200_200/0/1617267809315?e=2147483647&v=beta&t=u-1yUx5xYTkSEisAt7XT1LuKwzJ0F4iNZEtT3fZaQos'),
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      left: 16,
+                      top: 16,
+                    ),
+                    height: 16,
+                    width: 200,
+                    child: const Text(
+                      'Lucman M. Abdulrachman',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(
+                      left: 16,
+                      top: 8,
+                    ),
+                    width: 170,
+                    child: Text(
+                      'University Instructor I',
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                ],
               )
             ],
           ),
-        )
-      ],
-    ),
-    bottomNavigationBar: NavigationBar(destinations: [
+          const SizedBox(height: 16),
+          const Divider(height: 1.15),
+          GestureDetector(
+            child: const ProfileMenu(
+              item: 'Personal Data',
+              icon: Icons.person,
+            ),
+          ),
+          GestureDetector(
+            child: const ProfileMenu(
+              item: 'Settings',
+              icon: Icons.settings,
+            ),
+          ),
+          GestureDetector(
+            child: const ProfileMenu(
+              item: 'E-Statement',
+              icon: Icons.notes_sharp,
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Divider(height: 1.15),
+          GestureDetector(
+            child: const ProfileMenu(
+              item: 'FAQs',
+              icon: Icons.pending,
+            ),
+          ),
+          GestureDetector(
+            child: const ProfileMenu(
+              item: 'Our Handbook',
+              icon: Icons.drive_file_rename_outline,
+            ),
+          ),
+          GestureDetector(
+            child: const ProfileMenu(
+              item: 'E-Statement',
+              icon: Icons.notes_sharp,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Card(
+            margin: const EdgeInsets.only(right: 16, left: 16),
+            color: Colors.lightBlue,
+            child: Row(
+              children: [
+                Container(
+                    margin:
+                        const EdgeInsets.only(left: 25, bottom: 16, top: 16),
+                    height: 12,
+                    width: 12,
+                    color: Colors.transparent,
+                    child: const Icon(Icons.support_agent_outlined)),
+                Container(
+                  margin: const EdgeInsets.only(left: 16),
+                  
+                  color: Colors.transparent,
+                  child: const Text('Feel free to ask. We are ready to help.'),
+                )
+              ],
+            ),
+          ),
+          
+        ],
+      )),
+      bottomNavigationBar: NavigationBar(destinations: [
         TextButton(onPressed: () => {}, child: const Icon(Icons.home)),
         TextButton(onPressed: () => {}, child: const Icon(Icons.pin_invoke)),
         TextButton(onPressed: () => {}, child: const Icon(Icons.mail)),
         TextButton(onPressed: () => {}, child: const Icon(Icons.person))
       ]),
-      
     );
   }
 }
 
-class HomeMenu {
-  final String name;
-  final String icon;
-  HomeMenu({
-    required this.name,
-    required this.icon,
-  });
+class ProfileMenu extends StatelessWidget {
+  final String item;
+  final IconData icon;
+  const ProfileMenu({Key? key, required this.item, required this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      const SizedBox(height: 16),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              const SizedBox(
+                width: 16,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Icon(icon),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Container(
+                color: Colors.transparent,
+                child: Text(item,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.only(right: 16),
+            decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(5)),
+            child: const Icon(Icons.arrow_right),
+          ),
+        ],
+      )
+    ]);
+  }
 }
